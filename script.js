782f7eb29e20aa88ca171a9cb0b0151f1344d7af
Bruno Alves Lourenço <bruunieng@gmail.com>
2024-09-23 00:01:22 +0000

    Ajustes estilo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.querySelector('.glitch').addEventListener('mouseover', () => {
-    document.querySelector('.glitch').style.transform = 'rotate(5deg)';
-  });
-  
-  document.querySelector('.glitch').addEventListener('mouseleave', () => {
-    document.querySelector('.glitch').style.transform = 'rotate(0deg)';
-  });
-  
+  document.querySelector('.glitch').style.transform = 'rotate(5deg)';
+});
+
+document.querySelector('.glitch').addEventListener('mouseleave', () => {
+  document.querySelector('.glitch').style.transform = 'rotate(0deg)';
+});
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+});
